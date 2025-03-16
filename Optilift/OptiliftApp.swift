b9ca2162d5882114395afbd7c9cb028f570d7285
Jonathan <jonsandmann@gmail.com>
2025-03-15 21:28:14 -0400

    Initial Commit

---
+++ Optilift/OptiliftApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptiliftApp.swift
+//  Optilift
+//
+//  Created by Jon Sandmann on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct OptiliftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
